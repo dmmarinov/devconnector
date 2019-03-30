8f404e2e97cf46b08b457d1488589badfe5e5aa9
Dmitry Marinov <d.marinov@any.run>
2019-03-31 02:04:56 +0400

    User register API

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -7,13 +8,17 @@ const posts = require("./routes/api/posts");
 
 const app = express();
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // DB Config
 const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB
 mongoose
   .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("connected"))
+  .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => {
